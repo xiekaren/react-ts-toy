0046086fade14f86d6754a842c941095744e4edc
Karen Xie <karen.xie@myob.com>
2017-11-07 10:18:52 +1300

    jest enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+	const div = document.createElement("div");
+	ReactDOM.render(<App />, div);
+});
+
+it("renders a button", () => {
+	const component = shallow(<App />);
+	expect(component.find("button").length).toBe(1);
 });
