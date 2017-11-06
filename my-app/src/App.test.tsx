89af39231a4d8ec2a968ba302566861e7e90b707
Karen Xie <karen.xie@myob.com>
2017-11-07 11:32:52 +1300

    setup jest with enzyme

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders component', () => {
+  var component = shallow(<App/>);
+  expect(component.find('.button').length).toBe(1);
 });
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -14,6 +14,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button className="button"/>
       </div>
     );
   }
